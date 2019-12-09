35a24dfbd7b3d12f80be3bd5473414fba67bd428
Choi Seunghwan <49161475+Choi-Seunghwan@users.noreply.github.com>
2019-12-09 09:29:53 +0900

    Create h-index_다시보기.js

---
+++ programmers.co.kr/h-index_다시보기.js
@@ -0,0 +1,8 @@
+function solution(citations) {
+    var answer = 0;
+    const c = citations.sort( (a,b)=> a-b);
+    const half = c[parseInt(c.length/2)];
+    let val = c[half];
+    // 생각해보면 이상 인용의 길이로 계산하면 됐음. 나중에 다시 보기
+    for
+}
