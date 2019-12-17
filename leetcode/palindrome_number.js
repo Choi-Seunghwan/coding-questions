80b46dc89d7d8ba61092a2340849335ccd8a6180
Choi Seunghwan <49161475+Choi-Seunghwan@users.noreply.github.com>
2019-12-17 18:39:15 +0900

    Create palindrome_number.js

---
+++ leetcode/palindrome_number.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    if (x < 0 || ( x % 10 === 0 && x != 0))
+        return false;
+    
+    let result = 0;
+    while( x > result){
+        result = (result * 10) + (x % 10);
+        x = parseInt(x / 10);
+    }
+    
+    return x === result || x === parseInt(result / 10);
+};
