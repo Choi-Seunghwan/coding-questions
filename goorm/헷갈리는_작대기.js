abda77fec1f4ca2f4ce4e72c838ee78243383cbb
Choi-Seunghwan <choiseunghwan.tech@gmail.com>
2020-12-15 22:03:04 +0900

    commit

--- goorm/헷갈리는_작대기.js
+++ goorm/헷갈리는_작대기.js
@@ -2,40 +2,67 @@
 
 const readline = require("readline");
 const rl = readline.createInterface({
-	input: process.stdin,
-	output: process.stdout
+  input: process.stdin,
+  output: process.stdout,
 });
 
-function s(input){
-	let count1 = 0; // 1
-	let count2 = 0; // I
-	let count3 = 0; // l
-	let count4 = 0; // |
-	
-	for(let i = 0; i < input.length; i++){
-		switch(input[i]){
-			case '1':
-				++count1;
-				break;
-			case 'I':
-				++count2;
-				break;
-			case 'l':
-				++count3;
-				break;
-			case '|':
-				++count4;
-				break;
-			default:
-				break;
-		}
-	}
-	console.log(`${count1}\n${count2}\n${count3}\n${count4}`);
+function s(input) {
+  let count1 = 0; // 1
+  let count2 = 0; // I
+  let count3 = 0; // l
+  let count4 = 0; // |
+
+  for (let i = 0; i < input.length; i++) {
+    switch (input[i]) {
+      case "1":
+        ++count1;
+        break;
+      case "I":
+        ++count2;
+        break;
+      case "l":
+        ++count3;
+        break;
+      case "|":
+        ++count4;
+        break;
+      default:
+        break;
+    }
+  }
+  console.log(`${count1}\n${count2}\n${count3}\n${count4}`);
 }
 
-rl.on("line", function(line) {
-	s(line);
-	rl.close();
-}).on("close", function() {
-	process.exit();
+rl.on("line", function (line) {
+  s(line);
+  rl.close();
+}).on("close", function () {
+  process.exit();
+});
+
+/////////
+
+// Run by Node.js
+
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.on("line", function (line) {
+  const count = { 1: 0, I: 0, l: 0, "|": 0 };
+
+  for (ch of line) {
+    if (ch === "1" || ch === "I" || ch === "l" || ch === "|") {
+      count[ch] += 1;
+    }
+  }
+  console.log(count["1"]);
+  console.log(count["I"]);
+  console.log(count["l"]);
+  console.log(count["|"]);
+  rl.close();
+}).on("close", function () {
+  process.exit();
 });
